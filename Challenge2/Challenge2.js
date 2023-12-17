f0aaf95330b8de06ea86fd81a4d9f5a2c5300ccc
Robins <robinmgk077@gmail.com>
2023-12-18 01:34:23 +0300

    initial commit

---
+++ Challenge2/Challenge2.js
@@ -0,0 +1,23 @@
+const consoleInput = prompt("Enter Speed");
+
+function speedDetector(speed) {
+  const speedLimit = 70;
+  const pointsPerDemerit = 5;
+  const pointsThreshold = 12;
+
+  if (speed < speedLimit) {
+    return "Ok";
+  } else {
+    const demeritPoints = Math.floor((speed - speedLimit) / pointsPerDemerit);
+
+    if (demeritPoints > pointsThreshold) {
+      return "License suspended";
+    } else {
+      return `Points: ${demeritPoints}`;
+    }
+  }
+}
+console.log(speedDetector(90));
+
+alert(`your demerit Points:${demeritPoints}`);
+// alert(`your student marks: ${consoleInput}\n Grade: ${grade}`);
