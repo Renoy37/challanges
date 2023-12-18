927c386594c51f91862fe9346aebf0231ab83023
Robins <robinmgk077@gmail.com>
2023-12-18 13:19:34 +0300

    second commit

---
+++ Challenge3/challenge3.js
@@ -0,0 +1,30 @@
+// function to calculate payee
+
+function payee(pay) {
+  if (pay <= 24000) {
+    return console.log(pay * 0.1);
+  } else if (pay >= 24001 && pay <= 32333) {
+    return console.log(pay * 0.25);
+  } else if (pay >= 32334 && pay <= 500000) {
+    return console.log(pay * 0.3);
+  } else if (pay >= 500001 && pay <= 800000) {
+    return console.log(pay * 0.325);
+  } else if (pay >= 800000) {
+    return console.log(pay * 0.35);
+  } else {
+    return console.log("invalid value");
+  }
+}
+payee(500200);
+
+
+// function to calculate nssf
+function rates (tire) {
+  if (tire <= 6000) {
+    return console.log("tire1:" ,tire * 0.06)
+  }
+  if (tire >= 6001 && tire <= 18000 ) {
+    return console.log("tire2:" ,tire * 0.06)
+  }
+}
+rates(3000)
