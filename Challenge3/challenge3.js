9a52aa444d208871eb7e34999d49936222916d8f
Robins <robinmgk077@gmail.com>
2023-12-20 13:14:38 +0300

    Assignment completed

--- Challenge3/challenge3.js
+++ Challenge3/challenge3.js
@@ -1,30 +1,83 @@
-// function to calculate payee
+function calculateNetSalary(basicSalary, benefits) {
+  // Calculating the gross salary
+  const grossSalary = basicSalary + benefits;
 
-//takes in pay and calculates the payee based on the conditions
-function payee(pay) {
-  if (pay <= 24000) {
-    return console.log(pay * 0.1);
-  } else if (pay >= 24001 && pay <= 32333) {
-    return console.log(pay * 0.25);
-  } else if (pay >= 32334 && pay <= 500000) {
-    return console.log(pay * 0.3);
-  } else if (pay >= 500001 && pay <= 800000) {
-    return console.log(pay * 0.325);
-  } else if (pay >= 800000) {
-    return console.log(pay * 0.35);
+  //calculating the PAYE (Tax)
+  let tax = 0;
+  if (grossSalary <= 24000) {
+    tax = grossSalary * 0.1;
+  } else if (grossSalary <= 32333) {
+    tax = grossSalary * 0.25;
+  } else if (grossSalary <= 500000) {
+    tax = grossSalary * 0.3;
+  } else if (grossSalary <= 800000) {
+    tax = grossSalary * 0.325;
   } else {
-    return console.log("invalid value");
+    tax = grossSalary * 0.35;
   }
-}
-payee(500200);
 
-// function to calculate nssf deductions
-function rates(tier) {
-  if (tier <= 6000) {
-    return console.log("tier1:", tier * 0.06);
+  //calculating NHIF Deductions
+  let nhifDeduction = 0;
+  if (grossSalary <= 5999) {
+    nhifDeduction = 150;
+  } else if (grossSalary <= 7999) {
+    nhifDeduction = 300;
+  } else if (grossSalary <= 11999) {
+    nhifDeduction = 400;
+  } else if (grossSalary <= 14999) {
+    nhifDeduction = 500;
+  } else if (grossSalary <= 19999) {
+    nhifDeduction = 600;
+  } else if (grossSalary <= 24999) {
+    nhifDeduction = 750;
+  } else if (grossSalary <= 29999) {
+    nhifDeduction = 850;
+  } else if (grossSalary <= 34999) {
+    nhifDeduction = 900;
+  } else if (grossSalary <= 39999) {
+    nhifDeduction = 950;
+  } else if (grossSalary <= 44999) {
+    nhifDeduction = 1000;
+  } else if (grossSalary <= 49999) {
+    nhifDeduction = 1100;
+  } else if (grossSalary <= 59999) {
+    nhifDeduction = 1200;
+  } else if (grossSalary <= 69999) {
+    nhifDeduction = 1300;
+  } else if (grossSalary <= 79999) {
+    nhifDeduction = 1400;
+  } else if (grossSalary <= 89999) {
+    nhifDeduction = 1500;
+  } else if (grossSalary <= 99999) {
+    nhifDeduction = 1600;
+  } else if (grossSalary >= 100000) {
+    nhifDeduction = 1700;
   }
-  if (tier >= 6001 && tire <= 18000) {
-    return console.log("tier2:", tier * 0.06);
+
+  //calculating the NSSF Deductions
+  let nssfDeduction = 0;
+  if (basicSalary <= 6000) {
+    nssfDeduction = basicSalary * 0.06;
+  } else if (basicSalary <= 18000) {
+    nssfDeduction = basicSalary * 0.06;
   }
+
+  //calculating the net salary
+  const netSalary = grossSalary - (tax + nhifDeduction + nssfDeduction);
+
+  // including all Values to be Returned
+  return {
+    grossSalary,
+    tax,
+    nhifDeduction,
+    nssfDeduction,
+    netSalary,
+  };
 }
-rates(3000);
+
+// passing in values to test the function(s)
+const basicSalary = 100000;
+const benefits = 70000;
+const salaryDetails = calculateNetSalary(basicSalary, benefits);
+
+console.log(salaryDetails);
--- README.md
+++ README.md
@@ -11,6 +11,14 @@ function that prompts the user to input student marks. The input should be betwe
 contains:index.html
 problem.js
 
+# To run:
+
+1.open index.html
+2.open on live server
+3.enter values on the console input
+4.press ok
+5.answer will be displayed on alert box
+
 # Challenge 2: Speed Detector (Toy Problem)
 
 a program that takes the speed of a car as input e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
@@ -20,9 +28,23 @@ For example, if the speed is 80, it should print: “Points: 2”. If the driver
 contains:challenge1.html
 challenge1.js
 
+# To run:
+
+1.Open terminal
+2.run nvm use node
+3.run node challange3.js
+answer will display on terminal
+
 # Challenge 3: Net Salary Calculator (Toy Problem)
 
 a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits. Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary.
 
 challenge1:challenge2.html
 challenge2.js
+
+# To run:
+
+1.Open terminal
+2.run nvm use node
+3.run node challange3.js
+answer will display on terminal
